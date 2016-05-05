8ebee9996de8eb239334976e1e93ff08150edacc
dfernandez <damiancerero@us.es>
2016-05-05 19:13:58 +0200

    Bajado tamaño cache para menor consumo de RAM

--- src/main/scala/efficiency/DistributionCache.scala
+++ src/main/scala/efficiency/DistributionCache.scala
@@ -1,9 +1,7 @@
 package efficiency
 
-import ClusterSchedulingSimulation.Job
+import java.util.Collections
 
-import scala.collection.immutable.Queue
-import scala.collection.mutable
 
 /**
   * Created by dfernandez on 18/2/16.
@@ -11,18 +9,18 @@ import scala.collection.mutable
 object DistributionCache {
 
   // Key: Mean, Deviation, Threshold -> Value: inverseCumulativeProbability
-  var normalDistributionCache =  new LFUCache[Tuple3[Double, Double, Double], Double](10000, 0.3f)
+  var normalDistributionCache =  Collections.synchronizedMap(new LFUCache[Tuple3[Double, Double, Double], Double](1500, 0.3f))
   var normalDistributionCacheCalls = 0
   var normalDistributionCacheMiss = 0
   def normalDistributionCacheHits = normalDistributionCacheCalls - normalDistributionCacheMiss
   // Key: Alpha, Beta, Threshold -> Value: cumulativeProbability(Threshold)
-  var gammaDistributionCache =  new LFUCache[Tuple3[Double, Double, Double], Double](50000, 0.3f)
+  var gammaDistributionCache =  Collections.synchronizedMap(new LFUCache[Tuple3[Double, Double, Double], Double](15000, 0.3f))
   var gammaDistributionCacheCalls = 0
   var gammaDistributionCacheMiss = 0
   def gammaDistributionCacheHits = gammaDistributionCacheCalls - gammaDistributionCacheMiss
   // Key: List[Long], Tuple6[Double] 1- Interarrival avg, 2- Interarrival stddev, 3 - mem avg, 4 - mem stddev
   // 5 - cpu avg, 6- cpu stddev
-  var jobAttributesCache = new LFUCache[Seq[Long], Tuple6[Double, Double, Double, Double, Double, Double]](10000, 0.3f)
+  var jobAttributesCache = Collections.synchronizedMap(new LFUCache[Seq[Long], Tuple6[Double, Double, Double, Double, Double, Double]](1500, 0.3f))
   var jobAttributesCacheCalls = 0
   var jobAttributesCacheMiss = 0
   def jobAttributesCacheHits = jobAttributesCacheCalls - jobAttributesCacheMiss
